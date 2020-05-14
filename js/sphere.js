7e97d8d32b0331ce66e9d8c8eafb247c7335952a
ammaradeel7 <51586160+ammaradeel7@users.noreply.github.com>
2020-05-14 14:13:40 +0500

    Fixed Header and Background image, added cool animation

---
+++ js/sphere.js
@@ -0,0 +1,54 @@
+/**
+ * Draws a particle sphere/spiral on canvas.
+ * 
+ * @author Hakim El Hattab | http://hakim.se
+ */
+(function(){
+	var d = document,
+		canvas = d.body.appendChild( d.createElement( 'canvas' ) ),
+		context = canvas.getContext( '2d' ),
+		time = 0,
+		w = 1,
+		h = 1,
+		cos = Math.cos,
+		sin = Math.sin,
+		PI = Math.PI;
+	
+	function resize() {
+        canvas.width = w = 650;
+		canvas.height = h = 650;
+		canvas.style.left = "600px";
+        canvas.style.top = "350px";
+        canvas.style.position = "absolute";
+	}
+
+	// Monitor browser resize
+	addEventListener( 'resize', resize, false );
+
+	// Initial size
+	resize();
+	
+	// The main animation loop
+	setInterval( function() {
+		context.clearRect( 0, 0, w, h );
+        context.fillStyle = 'rgba(0,255,255,.5)';
+        //rgba(250, 0, 0, 1)';
+        context.globalCompositeOperation = 'lighter';
+		
+		time += .1;
+		
+		// The number of particles to generate
+        i =10000;
+        //10500
+		
+		while( i-- ) {
+			// The magic
+			r = ( ( w + h ) * 0.4 ) * ( cos( ( time + i ) * ( .05 + ( ( sin(time*0.00002) / PI ) * .2 ) ) ) / PI );
+			
+			context.fillRect( sin(i) * r + (w/2), 
+							  cos(i) * r + (h/2), 
+							  1, 
+							  1 );
+		}
+	}, 16 );
+})()
